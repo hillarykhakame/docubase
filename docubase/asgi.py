3ba5df424079ba5c5ec87250741f7739c7b6234e
unknown <KONZA-VDI@GROUP26-025.vdi.konza>
2024-09-24 18:08:56 +0300

    Initial commit for DocuBase project

---
+++ docubase/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for docubase project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'docubase.settings')
+
+application = get_asgi_application()
